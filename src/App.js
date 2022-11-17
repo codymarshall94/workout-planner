fa3719a3106c94606a5e89a435fe72e3b0060236
Cody Marshall <codymstuntsx@gmail.com>
2022-11-17 15:54:11 -0800

    added routes for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Onboarding from './pages/PlanOptions/Onboarding';
+import Template from './pages/Template/Template';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Workout App
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/template" element={<Template />} />
+      </Routes>
     </div>
   );
 }
