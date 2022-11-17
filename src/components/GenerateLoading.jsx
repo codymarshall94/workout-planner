b072c19d5443784902d396dc6b6d8fc3e1c4d8c9
Cody Marshall <codymstuntsx@gmail.com>
2022-11-17 15:53:55 -0800

    loading component created

---
+++ src/components/GenerateLoading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../assets/css/generateloading.css"
+
+function GenerateLoading() {
+  return (
+    <div>
+        <h1>Generating workout template</h1>
+        <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default GenerateLoading
