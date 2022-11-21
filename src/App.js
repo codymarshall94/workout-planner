12b15effc22a3f42b51d588cb4d8737c80a43ba1
Cody Marshall <codymstuntsx@gmail.com>
2022-11-21 14:17:53 -0800

    added catch all route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Onboarding from './pages/PlanOptions/Onboarding';
 import Template from './pages/Template/Template';
+import Week from './pages/Week/Week';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Onboarding />} />
-        <Route path="/template" element={<Template />} />
+        <Route path="/week" element={<Week />} />
+        <Route path='/template' element={<Template />} />
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </div>
   );
