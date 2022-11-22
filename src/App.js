710c5a4a121e63fed2e261752069cd40870b2b88
Cody Marshall <codymstuntsx@gmail.com>
2022-11-22 13:53:14 -0800

    added new route for workout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Onboarding from './pages/PlanOptions/Onboarding';
 import Template from './pages/Template/Template';
 import Week from './pages/Week/Week';
 import { Routes, Route } from 'react-router-dom';
+import Workout from './pages/Workout/Workout';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Onboarding />} />
         <Route path="/week" element={<Week />} />
         <Route path='/template' element={<Template />} />
+        <Route path="/workout" element={<Workout />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </div>
