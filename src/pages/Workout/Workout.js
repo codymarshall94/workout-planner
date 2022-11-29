37138a25956a47f54be102a849aad479fce7948b
Cody Marshall <codymstuntsx@gmail.com>
2022-11-29 14:41:27 -0800

    axios call to display workouts in db

--- src/pages/Workout/Workout.js
+++ src/pages/Workout/Workout.js
@@ -1,43 +1,61 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router";
+import RepsInput from "./RepsInput";
 
 function Workout() {
-  const savedWorkout = useSelector((state) => state.workout.value);
+  const [savedWorkout, setSavedWorkout] = useState([]);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/workout")
+      .then((res) => {
+        setSavedWorkout(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <>
+    <div className="w-100">
       <button className="btn btn-primary" onClick={() => navigate("/template")}>
         Edit
       </button>
       <div className="container">
-        {savedWorkout.exercises.map((exercise) => (
-          <div className="row border mb-5">
-            <div className="col border-bottom bg-dark text-light p-3">
-              <h5>{exercise.exerciseName}</h5>
-            </div>
-            <div className="row">
-              <div className="col">
-                <h5>
-                  {exercise.sets}x{exercise.reps}
-                </h5>
+        {savedWorkout.map((exercise) => (
+          <div>
+            {exercise.exercises.map((exercise) => (
+              <div className="row border mb-5">
+                <div className="col border-bottom bg-dark text-light p-3">
+                  <h5>{exercise.exerciseName}</h5>
+                </div>
+                <div className="row p-2">
+                  <div className="col">
+                    <h5>
+                      {exercise.sets}x{exercise.reps}
+                    </h5>
+                  </div>
+                  <div className="col">
+                    <h5>{exercise.weight}lbs</h5>
+                  </div>
+                  <div className="col">
+                    <h5>{exercise.rest}</h5>
+                  </div>
+                  <div className="col">
+                    <h5>{exercise.rir}rir</h5>
+                  </div>
+                </div>
+                <RepsInput sets={exercise.sets} />
               </div>
-              <div className="col">
-                <h5>{exercise.weight}lbs</h5>
-              </div>
-              <div className="col">
-                <h5>{exercise.rest}</h5>
-              </div>
-              <div className="col">
-                <h5>{exercise.rir}rir</h5>
-              </div>
-            </div>
-            {/* render rows here based on amount of exercise.sets*/}
+            ))}
           </div>
         ))}
       </div>
-    </>
+      <button className="btn btn-success">Complete</button>
+    </div>
   );
 }
 
