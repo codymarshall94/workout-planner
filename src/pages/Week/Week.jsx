48632b90f6a74fbf104bffff1b33362afcab66e9
Cody Marshall <codymstuntsx@gmail.com>
2022-12-01 13:20:42 -0800

    functions to change weeks

--- src/pages/Week/Week.jsx
+++ src/pages/Week/Week.jsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./week.css";
 import { useNavigate } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { changeDay } from "../../redux/reducers/daySlice";
 
-const days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-
 function Week() {
-  const workoutSaved = useSelector((state) => state.workout.value);
+  const [week, setWeek] = useState([]);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    function getDaysInWeek() {
+      let days = [];
+      const today = new Date();
+      const day = today.getDay();
+      const date = today.getDate();
+      for (let i = 0; i < 7; i++) {
+        const dayToAdd = new Date(today);
+        dayToAdd.setDate(date - day + i);
+        days.push(dayToAdd.toDateString());
+      }
+      setWeek(days);
+    }
+    getDaysInWeek();
+  }, []);
+
   const handleDayClick = (day) => {
     dispatch(changeDay(day));
-    if (workoutSaved.length > 0 || workoutSaved !== undefined) {
-      navigate("/workout");
-    } else {
-      navigate("/template");
-    }
+    navigate("/workout");
+  };
+
+  const changePreviousWeek = () => {
+    const prevWeek = week.map((day) => {
+      const newDay = new Date(day);
+      newDay.setDate(newDay.getDate() - 7);
+      return newDay.toDateString();
+    });
+    setWeek(prevWeek);
+  };
+
+  const changeNextWeek = () => {
+    const nextWeek = week.map((day) => {
+      const newDay = new Date(day);
+      newDay.setDate(newDay.getDate() + 7);
+      return newDay.toDateString();
+    });
+    setWeek(nextWeek);
   };
 
   return (
@@ -38,17 +58,29 @@ function Week() {
       <h2 className="bg-dark text-light p-2">Week 1</h2>
       <div className="container">
         <div className="row">
-          {days.map((day) => (
+          {week.map((day) => (
             <div
               onClick={() => handleDayClick(day)}
               key={day}
               className="col col-med-4 template-day-card"
             >
-              <h5 className="template-day-pill">{day}</h5>
-              <p className="template-day-card-text">Training</p>
+              <h5 className="template-day-pill">{day.substr(0, 3)}</h5>
+              <span className="template-day-card-text">{day.substr(3)}</span>
             </div>
           ))}
         </div>
+        <button
+          className="btn btn-primary m-2"
+          onClick={() => changePreviousWeek()}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary m-2"
+          onClick={() => changeNextWeek()}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
